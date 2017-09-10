731726c5ab39a012302b66c3a756bdcba95e7671
Hana Pearlman <hanapearlman@nichols-ve503-0279.apn.wlan.upenn.edu>
2017-09-09 20:56:34 -0400

    timer

--- app/src/main/java/com/example/hanapearlman/gifsort/GameActivity.java
+++ app/src/main/java/com/example/hanapearlman/gifsort/GameActivity.java
@@ -3,6 +3,7 @@ package com.example.hanapearlman.gifsort;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.RequiresApi;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -20,6 +21,7 @@ import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -62,6 +64,26 @@ public class GameActivity extends AppCompatActivity {
     int score;
     Context context;
 
+    TextView timerTextView;
+    long startTime = 0;
+
+    //runs without a timer by reposting this handler at the end of the runnable
+    Handler timerHandler = new Handler();
+    Runnable timerRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            long millis = System.currentTimeMillis() - startTime;
+            int seconds = (int) (millis / 1000);
+            int minutes = seconds / 60;
+            seconds = seconds % 60;
+
+            timerTextView.setText(String.format("%d:%02d", minutes, seconds));
+
+            timerHandler.postDelayed(this, 500);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -78,11 +100,6 @@ public class GameActivity extends AppCompatActivity {
         ivGif = (ImageView) findViewById(R.id.ivGif);
         score = 0;
 
-//        Glide.with(this)
-//                .load("https://media.giphy.com/media/RTvv8ST7rYUec/giphy.gif")
-//                .asGif()
-//                .into(ivGif);
-
         cvGif.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -90,8 +107,11 @@ public class GameActivity extends AppCompatActivity {
                 return true;
             }
         });
+        timerTextView = (TextView) findViewById(R.id.tvTime);
         tvScore = (TextView) findViewById(R.id.tvScore);
         tvScore.setText("Score: " + score);
+        startTime = System.currentTimeMillis();
+        timerHandler.postDelayed(timerRunnable, 0);
     }
 
     private void fillCategories() {
@@ -298,6 +318,7 @@ public class GameActivity extends AppCompatActivity {
             }
         }
         populateCategoryNames();
+        Collections.shuffle(gifSet);
     }
 
     public void getGiphysFromCategory(final String category) {
@@ -348,11 +369,19 @@ public class GameActivity extends AppCompatActivity {
 
     public void showNextGif() {
         gifSet.remove(0);
-        Glide.with(this)
-                .load(gifSet.get(0).getUrl())
-                .asGif()
-                .override(gifSet.get(0).width, gifSet.get(0).height)
-                .into(ivGif);
-        Log.i("NEWGIF", gifSet.get(0).getUrl());
+        if (gifSet.size() == 0) {
+            timerHandler.removeCallbacks(timerRunnable);
+            long tEnd = System.currentTimeMillis();
+            long tDelta = tEnd - startTime;
+            double elapsedSeconds = tDelta / 1000.0;
+            //TODO: handle game over somehow
+        } else {
+            Glide.with(this)
+                    .load(gifSet.get(0).getUrl())
+                    .asGif()
+                    .override(gifSet.get(0).width, gifSet.get(0).height)
+                    .into(ivGif);
+            Log.i("NEWGIF", gifSet.get(0).getUrl());
+        }
     }
 }
